a0ab5b0d286c9d6888d289b02debd553f2460177
WikiBear84 <wikibear84@naver.com>
2017-10-03 08:13:39 +0900

    master commit

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("update!!!");
-		System.out.println("Tell his World~");
+		System.out.println("Tell master World~");
 	}
 }
